675ae4c6b34753d21e86da0e7f8af118f8f65356
Muhammad Awais Atta <awaisatta999@gmail.com>
2026-04-16 17:26:24 +0200

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,58 @@
+export const siteConfig = {
+  name: "Muhammad Awais Atta",
+  title: "Student | Software Engineer | Tech Enthusiast",
+  description: "Portfolio website of Muhammad Awais Atta",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "awaisatta999@gmail.com",
+    linkedin: "https://linkedin.com/in/muhammad-awais-atta",
+    discord: "https://discord.com/users/339530782651580416",
+    github: "https://github.com/awaisatta",
+  },
+  aboutMe:
+    "Engaged and responsible student with experience in warehouse and administrative work. Strong interest in leadership, with a focus on organization, attention to detail, and problem-solving. Currently pursuing a degree in Computer Science, with a passion for software development and technology. Eager to apply my skills and knowledge in real-world projects and contribute to innovative solutions.",
+  skills: ["Prompt Engineering", "Python", "Docker"],
+  projects: [
+    {
+      name: "Express Vapes Ecommerce",
+      description:
+        "Custom-built ecommerce platform with advanced product filtering, AJAX-driven cart system, optimized checkout flow, and performance-focused frontend architecture.",
+      link: "https://theexpressvapes.com",
+      skills: ["Laravel", "PHP", "MySQL", "JavaScript", "AJAX"],
+    }
+    
+  ],
+  experience: [
+   {
+      company: "Stadsmissionen Second Hand",
+      title: "Warehouse Assistant",
+      dateRange: "Jul 2023 - Aug 2023",
+      bullets: [
+        "Oversaw inventory management and stock organization in a fast-paced retail environment",
+        "Executed product pricing and labeling to ensure accurate and efficient merchandising",
+        "Improved store layout and accessibility, enhancing overall customer experience",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Chalmers University of Technology",
+      degree: "Bachelor of Science in Computer Science",
+      dateRange: "2025 - Present",
+      achievements: [
+      "Currently pursuing a Bachelor's degree in Computer Science",
+      "Developing strong foundations in programming, algorithms, and system design",
+      "Building real-world projects in full-stack development",
+    ],
+    },
+    {
+      school: "Polhemsgymnasiet",
+      degree: "High School Diploma, Natural Science Program",
+      dateRange: "Aug 2022 - Jun 2025",
+      achievements: [
+        "Specialized in mathematics and physics with a focus on logical thinking and problem-solving",
+        "Developed strong skills in critical thinking and independent work",
+      ],
+    }
+  ],
+};
